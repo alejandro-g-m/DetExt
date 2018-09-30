00bc7a3f42457c45e1ccd7946677224370c2a027
AlejandroGallego <alejandrogallegomartinez@gmail.com>
2018-09-30 20:34:26 +0200

    Add new needed imports

--- dns_attacks_detection.py
+++ dns_attacks_detection.py
@@ -2,14 +2,15 @@ from feature_vector_creation import *
 import matplotlib.pyplot as plt
 import numpy as np
 from pandas.plotting import scatter_matrix
-from sklearn.model_selection import StratifiedShuffleSplit, cross_val_predict
+from sklearn.model_selection import StratifiedShuffleSplit, cross_val_predict, RandomizedSearchCV, GridSearchCV
 from sklearn.linear_model import SGDClassifier, LogisticRegression
 from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier, export_graphviz
 from sklearn.neighbors import KNeighborsClassifier
+from scipy.stats import randint
 
-# dns_features = create_feature_vector_from_log_file('3JUL.log', extract_features_reduced)
+# dns_features = create_feature_vector_from_log_file('3JUL.log', extract_features_with_letters_and_numbers)
 # print(dns_features[:100])
 def plot_precision_recall_vs_threshold(precisions, recalls, thresholds):
     plt.plot(thresholds, precisions[:-1], 'b--', label="Precision", linewidth=2)
