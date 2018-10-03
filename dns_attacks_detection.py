735ccf78e17577abe8a08fb71ea5e93205a2f881
AlejandroGallego <alejandrogallegomartinez@gmail.com>
2018-10-03 19:59:37 +0200

    Add function to split train and test sets

--- dns_attacks_detection.py
+++ dns_attacks_detection.py
@@ -13,6 +13,17 @@ from sklearn.ensemble import VotingClassifier, RandomForestClassifier, ExtraTree
 
 # dns_features = create_feature_vector_from_log_file('3JUL.log', extract_features_with_letters_and_numbers)
 # print(dns_features[:100])
+def split_train_and_test_sets(data, target_variable):
+    """
+    Splits a given feature vector in a 80% train set and 20% test set.
+    Uses Stratified Sampling with the variable passed in "target_variable"
+    """
+    split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=13)
+    for train_index, test_index in split.split(data, data[target_variable]):
+        train_set = data.loc[train_index]
+        test_set = data.loc[test_index]
+    return train_set, test_set
+
 def plot_precision_recall_vs_threshold(precisions, recalls, thresholds):
     plt.plot(thresholds, precisions[:-1], 'b--', label="Precision", linewidth=2)
     plt.plot(thresholds, recalls[:-1], 'g-', label="Recall", linewidth=2)
