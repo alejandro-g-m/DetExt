03f5c8dda75168df9a1f9dafd877c7403cbba9a2
Alejandro <alejandrogallegomartinez@gmail.com>
2019-10-01 14:55:54 +0200

    Add --include tag to coverage run. Add HTMl report generation.  and  set as variables at the beginning of the processing

--- unit_tests/run_tests_with_coverage.sh
+++ unit_tests/run_tests_with_coverage.sh
@@ -1,6 +1,9 @@
 branch=false
 missing=false
 all=false
+core_files="../feature_vector_creation.py,../dns_attacks_detection.py,../parse_BRO_log_file.py,../live_sniffer.py"
+test_files="unit_tests.py,unit_tests_feature_vector_creation.py,unit_tests_dns_attacks_detection.py"
+
 
 while [ "$1" != "" ]; do
     case $1 in
@@ -13,23 +16,26 @@ while [ "$1" != "" ]; do
     shift
 done
 
-# Shows (or not) branch coverage (conditional posibilities)
+# Shows branch coverage (conditional posibilities)
 if [ "$branch" = true ] ; then
-    coverage run --branch unit_tests.py
+    coverage run --include="$core_files,$test_files" --branch unit_tests.py
 else
-    coverage run unit_tests.py
+    coverage run --include="$core_files,$test_files" unit_tests.py
 fi
 
 # Shows final report for all files or only the main code ones
 if [ "$all" = true ] ; then
-    files="../feature_vector_creation.py ../dns_attacks_detection.py unit_tests.py unit_tests_feature_vector_creation.py unit_tests_dns_attacks_detection.py"
+    report_files="$core_files,$test_files"
 else
-    files="../feature_vector_creation.py ../dns_attacks_detection.py"
+    report_files="$core_files"
 fi
 
-# Shows lines (or not) that have not been run in final report
+# Shows lines that have not been run in final report
 if [ "$missing" = true ] ; then
-    coverage report -m $files
+    coverage report -m --include="$report_files"
 else
-    coverage report $files
+    coverage report --include="$report_files"
 fi
+
+# Generate HTML report
+coverage html --title="Test coverage DetExt" --include="$report_files"
